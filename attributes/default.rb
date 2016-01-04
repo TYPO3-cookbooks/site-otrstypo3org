78725129577a91b9d955bebd73af9e4686eb3bf8
Steffen Gebert <steffen.gebert@typo3.org>
2016-01-04 12:04:03 +0100

    Add Kernel/Config.pm settings

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,10 @@
 default['otrs']['version'] = "5.0.5"
 default['otrs']['fqdn'] = "otrs.typo3.org"
 
+default['otrs']['kernel_config']['email'] = "admin@typo3.org"
+default['otrs']['kernel_config']['organization'] = "TYPO3 Association"
+default['otrs']['kernel_config']['system_id'] = 74
+
 default['site-otrstypo3org']['ssl_certificate'] = "wildcard.typo3.org"
 default['otrs']['apache']['ssl_cert'] = node['ssl_certificates']['path'] + "/wildcard.typo3.org.crt"
 default['otrs']['apache']['ssl_key'] = node['ssl_certificates']['path'] + "/wildcard.typo3.org.key"
