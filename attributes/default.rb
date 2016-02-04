d7dcc129aed47be4222bacac4852a813ea1b932b
Steffen Gebert <steffen.gebert@typo3.org>
2016-02-04 23:05:10 +0100

    Revert "Upgrade to OTRS 5.0.6"
    This reverts commit f698ba8d38ad69481480bcb1cec52b75cbbb09e7.
    
    OTRS 5.0.6 introduces a bug that results in a warning mail being sent to us
    whenever a new mail is fetched.
    http://bugs.otrs.org/show_bug.cgi?id=11827
    
    Revert back to 5.0.5.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['otrs']['version'] = "5.0.6"
+default['otrs']['version'] = "5.0.5"
 default['otrs']['fqdn'] = "otrs.typo3.org"
 
 default['otrs']['kernel_config']['email'] = "admin@typo3.org"
