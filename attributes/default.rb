2496bb11693564d71002bb2b87c8357d1876affe
Steffen Gebert <steffen.gebert@typo3.org>
2016-03-20 10:50:12 +0100

    Bump OTRS version to 5.0.8

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['otrs']['version'] = "5.0.7"
+default['otrs']['version'] = "5.0.8"
 default['otrs']['fqdn'] = "otrs.typo3.org"
 
 default['otrs']['kernel_config']['email'] = "admin@typo3.org"
